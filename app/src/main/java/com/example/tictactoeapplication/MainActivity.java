955adda1bb9a6636251f9a3ecfe17e292ee63588
Arsh <arshsyed7985@gmail.com>
2024-06-27 17:20:18 +0530

    first commit

---
+++ app/src/main/java/com/example/tictactoeapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tictactoeapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+//first commit
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void playButtonClick(View view){
+        Intent intent = new Intent(this, PlayerSetup.class);
+        startActivity(intent);
+
+    }
+}
